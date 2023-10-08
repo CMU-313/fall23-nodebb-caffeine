f313fab008413f0833bb21ae4c309a98d4701a1e
Abby Chen <archen@andrew.cmu.edu>
2023-10-07 21:41:52 -0500

    Added functionality for changing username based on mode

--- src/user/anonymous.js
+++ src/user/anonymous.js
@@ -1,3 +1,23 @@
+const user = require('index.js');
+// returns true if mode is on, false if not
+const mode = user.getMode()
+
+// change username based on anonymous mode
+const setAnonymousUsername = () => {
+    const username = document.getElementById('show-username');
+    if (username) {
+        if (mode === 'true') {
+            // set username to "anonymous" when anonymous mode is on
+            username.textContent = 'anonymous';
+        } else {
+            // set username to original username
+            username.textContent = username.getAttribute('data-username');
+        }
+    }
+};
+
+setAnonymousUsername();
+
 // turn on anonymous mode if button is clicked
 const anonymousButton = document.getElementById('anonymous-mode');
 anonymousButton.addEventListener('click', function () {

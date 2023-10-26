60b74c80d8d96de9115611fb8c14a9e7cc5c4ee1
Abby Chen <archen@andrew.cmu.edu>
2023-10-26 15:59:28 -0400

    Updated json file environment variables

--- create_config.sh
+++ create_config.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 # Check that environment variables have been defined
-if [[ -z "us1-wealthy-rhino-41526.upstash.io+x" ]]; then
+if [[ -z "us1-wealthy-rhino-41526.upstash.iox" ]]; then
   # var is not defined
   echo "Error: REDIS_HOST is not defined!"
   exit 1
 fi
 
-if [[ -z "41526+x" ]]; then
+if [[ -z "41526x" ]]; then
   # var is not defined
   echo "Error: REDIS_PORT is not defined!"
   exit 1
 fi
 
-if [[ -z "aee6eb861d8240a0b1b70f49aec09262+x" ]]; then
+if [[ -z "aee6eb861d8240a0b1b70f49aec09262x" ]]; then
   # var is not defined
   echo "Error: REDIS_PASSWORD is not defined!"
   exit 1
 fi
 
-if [[ -z "https://fall23-nodebb-caffeine-q4slcgtxpa-uc.a.run.app+x" ]]; then
+if [[ -z "https://fall23-nodebb-caffeine-q4slcgtxpa-uc.a.run.appx" ]]; then
   # var is not defined
   echo "Error: DEPLOYMENT_URL is not defined!"
   exit 1
@@ -29,10 +29,10 @@ fi
 json_data=$(cat "/usr/src/app/config_template.json")
 
 # Update the JSON file with the environment variables
-json_data=$(jq --arg deployment_url "$DEPLOYMENT_URL" '.url = $deployment_url' <<< "$json_data")
-json_data=$(jq --arg host "$REDIS_HOST" '.redis.host = $host' <<< "$json_data")
-json_data=$(jq --arg port "$REDIS_PORT" '.redis.port = $port' <<< "$json_data")
-json_data=$(jq --arg password "$REDIS_PASSWORD" '.redis.password = $password' <<< "$json_data")
+json_data=$(jq --arg deployment_url "https://fall23-nodebb-caffeine-q4slcgtxpa-uc.a.run.app" '.url = $deployment_url' <<< "$json_data")
+json_data=$(jq --arg host "us1-wealthy-rhino-41526.upstash.iox" '.redis.host = $host' <<< "$json_data")
+json_data=$(jq --arg port "41526" '.redis.port = $port' <<< "$json_data")
+json_data=$(jq --arg password "aee6eb861d8240a0b1b70f49aec09262" '.redis.password = $password' <<< "$json_data")
 
 # Write the updated JSON file to config.json
 echo "$json_data" > "/usr/src/app/config.json"
